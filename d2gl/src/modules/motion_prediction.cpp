e5def78c74737bb016a510899aef2b0e46d7ca1d
Walter White <bayaraa_844@yahoo.com>
2023-02-22 17:34:02 +0900

    add license to files

--- d2gl/src/modules/motion_prediction.cpp
+++ d2gl/src/modules/motion_prediction.cpp
@@ -1,16 +1,37 @@
 /*
+	D2GL: Diablo 2 LoD Glide/DDraw to OpenGL Wrapper.
+	Copyright (C) 2023  Bayaraa
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+/*
 	This file is "partly" of D2DX.
 	https://github.com/bolrog/d2dx/blob/main/src/d2dx/UnitMotionPredictor.cpp
 	https://github.com/bolrog/d2dx/blob/main/src/d2dx/WeatherMotionPredictor.cpp
+
 	Copyright (C) 2021  Bolrog
+
 	D2DX is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
+
 	D2DX is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
+
 	You should have received a copy of the GNU General Public License
 	along with D2DX. If not, see <https://www.gnu.org/licenses/>.
 */
